7aeae6ee158f0f9fdebad3f2cc8e18aaef3254f1
Oshi Attanayaka <ishiaththanayaka43@gmail.com>
2025-05-02 10:07:27 +0530

    doc dashboard updated
    dashboard.cs and dashboard1.cs design updated..

--- TrustWell Hospital Doctor/Dashbord1.Designer.cs
+++ TrustWell Hospital Doctor/Dashbord1.Designer.cs
@@ -28,86 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.mobile = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.cuiPanel1 = new CuoreUI.Controls.cuiPanel();
+            this.cuiLabel1 = new CuoreUI.Controls.cuiLabel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cuiPanel2 = new CuoreUI.Controls.cuiPanel();
+            this.cuiPanel4 = new CuoreUI.Controls.cuiPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.cuiPanel1.SuspendLayout();
+            this.cuiPanel2.SuspendLayout();
+            this.cuiPanel4.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.AutoSize = true;
-            this.panel1.BackColor = System.Drawing.Color.LimeGreen;
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(177, 15);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(302, 100);
-            this.panel1.TabIndex = 0;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(267, 22);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Number of today\'s appointments";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(124, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 31);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "12";
-            // 
-            // panel2
-            // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.AutoSize = true;
-            this.panel2.BackColor = System.Drawing.Color.LimeGreen;
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Location = new System.Drawing.Point(619, 15);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(328, 100);
-            this.panel2.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(19, 46);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(34, 99);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(287, 22);
+            this.label3.Size = new System.Drawing.Size(358, 26);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Number of upcoming appointments";
+            this.label3.Text = "Number of completed appointments";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(131, 15);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(196, 49);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 31);
+            this.label4.Size = new System.Drawing.Size(34, 37);
             this.label4.TabIndex = 1;
-            this.label4.Text = "12";
+            this.label4.Text = "5";
             // 
             // dataGridView1
             // 
@@ -116,11 +81,12 @@
             this.number,
             this.name,
             this.mobile});
-            this.dataGridView1.Location = new System.Drawing.Point(27, 133);
+            this.dataGridView1.Location = new System.Drawing.Point(30, 312);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1763, 690);
+            this.dataGridView1.Size = new System.Drawing.Size(1983, 716);
             this.dataGridView1.TabIndex = 2;
             // 
             // number
@@ -144,37 +110,147 @@
             this.mobile.Name = "mobile";
             this.mobile.Width = 125;
             // 
+            // cuiPanel1
+            // 
+            this.cuiPanel1.Controls.Add(this.cuiLabel1);
+            this.cuiPanel1.Controls.Add(this.label7);
+            this.cuiPanel1.Location = new System.Drawing.Point(30, 17);
+            this.cuiPanel1.Name = "cuiPanel1";
+            this.cuiPanel1.OutlineThickness = 1F;
+            this.cuiPanel1.PanelColor = System.Drawing.SystemColors.ControlLight;
+            this.cuiPanel1.PanelOutlineColor = System.Drawing.SystemColors.ControlLight;
+            this.cuiPanel1.Rounding = new System.Windows.Forms.Padding(13);
+            this.cuiPanel1.Size = new System.Drawing.Size(722, 171);
+            this.cuiPanel1.TabIndex = 3;
+            // 
+            // cuiLabel1
+            // 
+            this.cuiLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.cuiLabel1.Content = "You\\ can\\ choose\\ a\\ patient\\ here\\ to\\ enter\\ into\\ their\\ details\\.";
+            this.cuiLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuiLabel1.ForeColor = System.Drawing.Color.SlateGray;
+            this.cuiLabel1.HorizontalAlignment = System.Drawing.StringAlignment.Near;
+            this.cuiLabel1.Location = new System.Drawing.Point(50, 94);
+            this.cuiLabel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cuiLabel1.Name = "cuiLabel1";
+            this.cuiLabel1.Size = new System.Drawing.Size(542, 72);
+            this.cuiLabel1.TabIndex = 11;
+            this.cuiLabel1.VerticalAlignment = System.Drawing.StringAlignment.Near;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.SlateGray;
+            this.label7.Location = new System.Drawing.Point(43, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(587, 58);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "DASHBOARD";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // cuiPanel2
+            // 
+            this.cuiPanel2.Controls.Add(this.label4);
+            this.cuiPanel2.Controls.Add(this.label3);
+            this.cuiPanel2.Location = new System.Drawing.Point(773, 17);
+            this.cuiPanel2.Name = "cuiPanel2";
+            this.cuiPanel2.OutlineThickness = 1F;
+            this.cuiPanel2.PanelColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(170)))), ((int)(((byte)(200)))));
+            this.cuiPanel2.PanelOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(170)))), ((int)(((byte)(200)))));
+            this.cuiPanel2.Rounding = new System.Windows.Forms.Padding(13);
+            this.cuiPanel2.Size = new System.Drawing.Size(416, 171);
+            this.cuiPanel2.TabIndex = 4;
+            // 
+            // cuiPanel4
+            // 
+            this.cuiPanel4.Controls.Add(this.label6);
+            this.cuiPanel4.Controls.Add(this.label8);
+            this.cuiPanel4.Location = new System.Drawing.Point(1210, 17);
+            this.cuiPanel4.Name = "cuiPanel4";
+            this.cuiPanel4.OutlineThickness = 1F;
+            this.cuiPanel4.PanelColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(170)))), ((int)(((byte)(200)))));
+            this.cuiPanel4.PanelOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(170)))), ((int)(((byte)(200)))));
+            this.cuiPanel4.Rounding = new System.Windows.Forms.Padding(13);
+            this.cuiPanel4.Size = new System.Drawing.Size(416, 171);
+            this.cuiPanel4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(43, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(352, 26);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Number of upcoming appointments";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Control;
+            this.label8.Location = new System.Drawing.Point(189, 49);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 37);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "12";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label1.Location = new System.Drawing.Point(25, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 29);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Pick the patient here";
+            // 
             // Dashbord1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cuiPanel4);
+            this.Controls.Add(this.cuiPanel2);
+            this.Controls.Add(this.cuiPanel1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Dashbord1";
             this.Size = new System.Drawing.Size(1810, 850);
             this.Load += new System.EventHandler(this.Dashbord1_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.cuiPanel1.ResumeLayout(false);
+            this.cuiPanel2.ResumeLayout(false);
+            this.cuiPanel2.PerformLayout();
+            this.cuiPanel4.ResumeLayout(false);
+            this.cuiPanel4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn number;
         private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.DataGridViewTextBoxColumn mobile;
+        private CuoreUI.Controls.cuiPanel cuiPanel1;
+        private CuoreUI.Controls.cuiPanel cuiPanel2;
+        private System.Windows.Forms.Label label7;
+        private CuoreUI.Controls.cuiPanel cuiPanel4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private CuoreUI.Controls.cuiLabel cuiLabel1;
     }
 }
