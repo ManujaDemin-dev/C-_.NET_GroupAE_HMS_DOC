b70162ee6769ab7014a1dad17463fa8046d7c188
Oshi Attanayaka <ishiaththanayaka43@gmail.com>
2025-05-08 19:40:54 +0530

    dashboard and old prescription updated
    layout of dashboard updated and old prescription is designed

---
+++ TrustWell Hospital Doctor/popUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrustWell_Hospital_Doctor
+{
+    public partial class popUp: Form
+    {
+        public popUp(string date, string docName, string prescription)
+        {
+            InitializeComponent();
+
+            label1.Text = "Date: " + date;
+            label2.Text = "Doctor Name: " + docName;
+            textBox1.Text =  prescription;
+        }
+
+        private void popUp_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
